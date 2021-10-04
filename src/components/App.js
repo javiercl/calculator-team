4d680fb6ad5b529dfd730587b81dd664277c1353
Javier Cisneros L <lucatero@hotmail.com>
2021-10-04 10:42:32 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+// import Display from './components/Display';
+// import ButtNum from './components/ButtNum';
+// import ButtOpe from './components/ButtOpe';
+// import ButtEqu from './components/ButtEqu';
+
+
+function App() {
+  const [values, setValues] = useState({
+    'result' : '0',   // valor que va en el display
+    'value' : '',     // número actual
+    'value_aux' : 0,  // valor 1 de una operacion
+    'operator' : '',  // operador tecleado
+    'number' : 0      // bandera indica si empezamos un nuevo número
+  });
+  
+  return (
+    <div class="fondo">
+      <p>Aqui van los componentes de la calculadora</p>
+      
+      {/* 
+
+      <Display values={values}/>
+      <ButtNum values={values} setValues={setValues}/>
+      <ButtEqu values={values} setValues={setValues}/>
+      <ButtOpe values={values} setValues={setValues}/> 
+      
+      */}
+
+    </div>
+  );
+}
+
+export default App;
