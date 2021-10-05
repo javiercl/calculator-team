024a8a9cb4e8fb12ec16b84a6d418dd3c7f09e03
AngelD99 <miguelangeltec99@gmail.com>
2021-10-05 11:43:00 -0500

    Modificacion al componente boton

--- src/components/ButtEqu.js
+++ src/components/ButtEqu.js
@@ -1,33 +1,38 @@
 import React from 'react';
 
 const Buttequ = ({values, setValues}) => {
+
+    const operacion = () =>{
+        let result=0;
+        switch(values.operador){
+            case '+':
+                result=values.value+values.value_aux;
+            break;
+            case '-':
+                result=values.value-values.value_aux;
+            break;
+            case '*':
+                result=values.value*values.value_aux;
+            break;
+            case '/':
+                result=values.value/values.value_aux;
+            break;
+        }
+
+        setValues({
+            'result':result,
+            'value' : values.value,     // número actual
+            'value_aux' : values.value_aux,  // valor 1 de una operacion
+            'operator' : values.operador,  // operador tecleado
+            'number' : values.number  
+        });
+    }
+
     return (
         <div>
              <button onClick={(e)=>{
                 e.preventDefault();
-                let result=0;
-                switch(values.operador){
-                    case '+':
-                        result=values.value+values.value_aux;
-                    break;
-                    case '-':
-                        result=values.value-values.value_aux;
-                    break;
-                    case '*':
-                        result=values.value*values.value_aux;
-                    break;
-                    case '/':
-                        result=values.value/values.value_aux;
-                    break;
-                }
-
-                setValues({
-                    'result':result,
-                    'value' : values.value,     // número actual
-                    'value_aux' : values.value_aux,  // valor 1 de una operacion
-                    'operator' : values.operador,  // operador tecleado
-                    'number' : values.number  
-                });
+                operacion();                
             }
             }>=</button>
         </div>
